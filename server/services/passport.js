499bed996944194fd627cc059c67512fff1689c2
yingzhuo <yingzhuo1994@gmail.com>
2022-06-22 12:47:18 -0500

    Avoid creating the existed user

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,19 +1,25 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
 
-const User = mongoose.model('users');
+const User = mongoose.model("users");
 
 passport.use(
-    new GoogleStrategy(
-      {
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: "/auth/google/callback",
-      },
-      (accessToken, refreshToken, profile, done) => {
-        new User({ googleId: profile.id}).save();
-      }
-    )
-  );
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          console.log('The user has existed.')
+        } else {
+          new User({ googleId: profile.id }).save();
+        }
+      });
+    }
+  )
+);
