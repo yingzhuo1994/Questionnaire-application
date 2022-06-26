24132c1ecc0b4b2ef4d27be342141f8e72e830cf
yingzhuo <yingzhuo1994@gmail.com>
2022-06-26 13:15:10 -0500

    Route Configuration

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
   return (
-  <div>
-    Hi There!
-    </div>);
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/surveys" element={<Dashboard />} />
+          <Route path="/surveys/new" element={<SurveyNew />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
