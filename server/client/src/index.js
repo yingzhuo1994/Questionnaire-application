f374a06890be131331a8e8e179bfee24b5262418
yingzhuo <yingzhuo1994@gmail.com>
2022-06-26 00:24:24 -0500

    Redux setup

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,6 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { applyMiddleware } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 
-import App from './components/App';
+import App from "./components/App";
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const store = configureStore({reducer: {}}, {}, applyMiddleware());
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
