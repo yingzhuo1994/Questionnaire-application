ffc0b15a2fd993d81bc7a394a32dacec39abaa78
yingzhuo <yingzhuo1994@gmail.com>
2022-06-26 23:31:17 -0500

    Refactor the app

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Header from "./Header";
@@ -6,19 +6,24 @@ const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
-const App = () => {
-  return (
-    <div className="container">
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/surveys" element={<Dashboard />} />
-          <Route path="/surveys/new" element={<SurveyNew />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
-};
+class App extends Component {
+  componentDidMount() {
+    
+  }
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/surveys" element={<Dashboard />} />
+            <Route path="/surveys/new" element={<SurveyNew />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
 
 export default App;
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,14 +1,15 @@
-import materializeCSS from 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/css/materialize.min.css';
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { applyMiddleware } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
+import reduxThunk from 'redux-thunk';
 
 import App from "./components/App";
 import reducers from './reducers';
 
-const store = configureStore({reducer: reducers}, {}, applyMiddleware());
+const store = configureStore({reducer: reducers}, {}, applyMiddleware(reduxThunk));
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
