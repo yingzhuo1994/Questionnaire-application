41f45e337fa2d89eb2c86f9a69d6c0f0a84f4c45
yingzhuo <yingzhuo1994@gmail.com>
2022-07-01 18:46:33 -0500

    Create Mailers

--- server/routes/surveyRoute.js
+++ server/routes/surveyRoute.js
@@ -1,8 +1,20 @@
-const requireLogin = require('../middlewares/requireLogin');
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
 
-module.exports = app => {
-    app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
-        
-    })
+const Survey = mongoose.model("surveys");
+
+module.exports = (app) => {
+  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateSent: Date.now(),
+    });
+  });
 };
