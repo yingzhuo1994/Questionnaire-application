278f76f814fa08412b8e5422f1bc13649606550d
yingzhuo <yingzhuo1994@gmail.com>
2022-07-03 18:19:59 -0500

    Fields from config

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -1,16 +1,23 @@
 // SurveyForm shows a form for a user to add input
+import _ from "lodash";
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
 import SurveyField from "./SurveyField";
 
+const FIELDS = [
+  { label: "Survey Title", name: "title" },
+  { label: "Subject Line", name: "subject" },
+  { label: "Email body", name: "body" },
+  { label: "Recipient List", name: "emails" },
+];
+
 class SurveyForm extends Component {
-    renderFields() {
-        return (
-            <div>
-                <Field type="text" name="title" component={SurveyField} />
-            </div>
-        )
-    }
+  renderFields() {
+    return _.map(FIELDS, ({label, name}) => {
+      return <Field key={name} component={SurveyField} type="text" label={label} name={name} />;
+    });
+  }
+
   render() {
     return (
       <div>
