92cdd696a792bb463034ab70f534957efac3e4d6
yingzhuo <yingzhuo1994@gmail.com>
2022-07-04 00:06:12 -0500

    Generalize Field Validation

--- server/client/src/components/surveys/SurveyField.js
+++ server/client/src/components/surveys/SurveyField.js
@@ -2,12 +2,12 @@
 // label and text input
 import React from "react";
 
-export default ({ input, label, meta: {error, touched} }) => {
+export default ({ input, label, meta: { error, touched } }) => {
   return (
     <div>
       <label>{label}</label>
-      <input {...input} />
-      {touched && error}
+      <input {...input} stype={{ marginBottom: '5px'}}/>
+      <div className="red-text" style={{ marginBottom: '20px' }}>{touched && error}</div>
     </div>
   );
 };
