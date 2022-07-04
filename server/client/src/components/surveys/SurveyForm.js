63804f22eb056facd85d04450d44a54d6310028a
yingzhuo <yingzhuo1994@gmail.com>
2022-07-04 18:06:12 -0500

    Toggle FormReview Visibility

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -31,9 +31,7 @@ class SurveyForm extends Component {
   render() {
     return (
       <div>
-        <form
-          onSubmit={this.props.handleSubmit((values) => console.log(values))}
-        >
+        <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
           {this.renderFields()}
           <Link to="/surveys" className="red btn-flat white-text">
             Cancel
@@ -53,9 +51,9 @@ function validate(values) {
 
   _.each(FIELDS, ({ name }) => {
     if (!values[name]) {
-        errors[name] = 'You must provide a value!';
+      errors[name] = "You must provide a value!";
     }
-  })
+  });
 
   errors.emails = validateEmails(values.emails || "");
 
--- server/client/src/components/surveys/SurveyNew.js
+++ server/client/src/components/surveys/SurveyNew.js
@@ -1,14 +1,25 @@
 // SurveyNew shows SurveyForm and SurveyReview
 import React, { Component } from "react";
 import SurveyForm from "./SurveyForm";
+import SurveyFormReview from "./SurveyFormReview";
 
 class SurveyNew extends Component {
+  state = { showFormReview: false };
+
+  renderContent() {
+    if (this.state.showFormReview === true) {
+      return <SurveyFormReview />;
+    }
+
+    return <SurveyForm onSurveySubmit={() => this.setState({
+      showFormReview: true
+    })} />;
+  }
+
   render() {
-    return (
-      <div>
-        <SurveyForm />
-      </div>
-    );
+    return <div>
+      {this.renderContent()}
+      </div>;
   }
 }
 
