5d9b48b24a4d7af035c56e2f300062bfee2704bd
yingzhuo <yingzhuo1994@gmail.com>
2022-07-06 13:35:40 -0500

    Finalize Review Fields

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -1,18 +1,31 @@
 // SurveyFormReview shows users their form inputs for review
+import _ from "lodash";
 import React from "react";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
+import formFields from "./formFields";
 
-const SurveyFormReview = ({ onCancel }) => {
+const SurveyFormReview = ({ onCancel, formValues }) => {
+  const reviewFields = _.map(formFields, ({name, label}) => {
+    return (
+      <div key={name}>
+        <label>{label}</label>
+        <div>{formValues[name]}</div>
+      </div>
+    );
+  });
   return (
     <div>
       <h5>Please confirm your entries</h5>
-      <button className="yellow darken-3 btn-flat" onClick={onCancel}>Back</button>
+      {reviewFields}
+      <button className="yellow darken-3 btn-flat" onClick={onCancel}>
+        Back
+      </button>
     </div>
   );
 };
 
 function mapStoreToProps(state) {
-  return { formValues: state.form.surveyForm.values};
+  return { formValues: state.form.surveyForm.values };
 }
 
 export default connect(mapStoreToProps)(SurveyFormReview);
---
+++ server/client/src/components/surveys/formFields.js
@@ -0,0 +1,7 @@
+const formFields = [
+  { label: "Survey Title", name: "title" },
+  { label: "Subject Line", name: "subject" },
+  { label: "Email body", name: "body" },
+  { label: "Recipient List", name: "emails" },
+];
+export default formFields;
