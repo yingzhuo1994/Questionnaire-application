1dfb603890dcf447e739ff6a21dc8e5850d581c5
yingzhuo <yingzhuo1994@gmail.com>
2022-07-06 18:34:23 -0500

    Post to surveys

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -49,7 +49,7 @@ function validate(values) {
     }
   });
 
-  errors.emails = validateEmails(values.emails || "");
+  errors.recipients = validateEmails(values.recipients || "");
 
   return errors;
 }
--- server/client/src/components/surveys/formFields.js
+++ server/client/src/components/surveys/formFields.js
@@ -2,6 +2,6 @@ const formFields = [
   { label: "Survey Title", name: "title" },
   { label: "Subject Line", name: "subject" },
   { label: "Email body", name: "body" },
-  { label: "Recipient List", name: "emails" },
+  { label: "Recipient List", name: "recipients" },
 ];
 export default formFields;
