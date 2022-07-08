65e0ecbcfa0dccc3dc043928cb8f90dc52fcdd3b
yingzhuo <yingzhuo1994@gmail.com>
2022-07-07 23:47:46 -0500

    Encode survey data, parse the route

--- server/routes/surveyRoute.js
+++ server/routes/surveyRoute.js
@@ -1,3 +1,6 @@
+const _ = require("lodash");
+const { Path } = require("path-parser");
+const { URL } = require("url");
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
@@ -7,8 +10,30 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
 
 module.exports = (app) => {
-  app.get('/api/surveys/thanks', (req, res) => {
-    res.send('Thanks for voting!');
+  app.get("/api/surveys/thanks", (req, res) => {
+    res.send("Thanks for voting!");
+  });
+
+  app.post("/api/surveys/webhooks", (req, res) => {
+    const events = _.chain(req.body)
+      .map(({ email, url }) => {
+        const p = new Path("/api/surveys/:surveyId/:choice");
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return {
+            email,
+            surveyId: match.surveyId,
+            choice: match.choice,
+          };
+        }
+      })
+      .compact()
+      .uniqBy("email", "surveyId")
+      .value();
+
+    console.log(events);
+
+    res.send({});
   });
 
   app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
@@ -38,7 +63,5 @@ module.exports = (app) => {
     } catch (err) {
       res.status(422).send(err);
     }
-
-    
   });
 };
