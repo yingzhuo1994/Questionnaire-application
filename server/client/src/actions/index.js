98b495c9d1023c61ef8a612cb9c4c6156709028a
yingzhuo <yingzhuo1994@gmail.com>
2022-07-09 13:27:10 -0500

    Wire react to redux

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,4 +1,4 @@
-import { FETCH_USER } from "./types";
+import { FETCH_USER, FETCH_SURVEYS } from "./types";
 
 const axios = require("axios");
 
@@ -16,6 +16,12 @@ export const handleToken = (token) => async (dispatch) => {
 export const submitSurvey = (values, navigate) => async (dispatch) => {
   const res = await axios.post("/api/surveys", values);
 
-  navigate('/surveys');
+  navigate("/surveys");
   dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const fetchSurveys = () => async (dispatch) => {
+  const res = await axios.get("/api/surveys");
+
+  dispatch({ type: FETCH_SURVEYS, payload: res.data });
 };
