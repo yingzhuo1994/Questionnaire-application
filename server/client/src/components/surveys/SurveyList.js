317a8aa112de272edfbf6fd0c9d54e7d518ca3ad
yingzhuo <yingzhuo1994@gmail.com>
2022-07-09 13:36:31 -0500

    Render a list of surveys

--- server/client/src/components/surveys/SurveyList.js
+++ server/client/src/components/surveys/SurveyList.js
@@ -1,22 +1,39 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { fetchSurveys } from '../../actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchSurveys } from "../../actions";
 
 class SurveyList extends Component {
-    componentDidMount() {
-        this.props.fetchSurveys();
-    }
-    render() {
-        return (
-            <div>
-                SurveyList
-            </div>
-        )
-    }
+  componentDidMount() {
+    this.props.fetchSurveys();
+  }
+
+  renderSurveys() {
+    return this.props.surveys.map((survey) => {
+      return (
+        <div className="card darken-1" key={survey._id}>
+          <div className="card-content">
+            <span className="card-title">{survey.title}</span>
+            <p>{survey.question}</p>
+            <p className="right">
+              Sent On: {new Date(survey.dateSent).toLocaleDateString()}
+            </p>
+          </div>
+          <div className="card-action">
+            <a>Yes: {survey.yes}</a>
+            <a>No: {survey.no}</a>
+          </div>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return <div>{this.renderSurveys()}</div>;
+  }
 }
 
-function mapStateToProps({surveys}) {
-    return {surveys};
+function mapStateToProps({ surveys }) {
+  return { surveys };
 }
 
-export default connect(mapStateToProps, {fetchSurveys})(SurveyList);
+export default connect(mapStateToProps, { fetchSurveys })(SurveyList);
